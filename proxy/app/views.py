20d1b0d3e2701941f1df6587ad5b890060305985
Ali Taba <ali79taba@gmail.com>
2024-06-20 13:51:06 +0330

    Fix: use json instead of cookie for requests

--- proxy/app/views.py
+++ proxy/app/views.py
@@ -1,9 +1,11 @@
+import json
 from datetime import timedelta
 
 import requests
 from django.http import HttpResponse
 from django.utils import timezone
-from django.views import View
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 class ProxySwitcher:
@@ -24,27 +26,37 @@ class ProxySwitcher:
         cls.latest_update = timezone.now()
 
     @classmethod
-    def call_request(cls, target_url, max_retries=2):
+    def call_request(cls, url, headers=None, method='GET', body=None, max_retries=2):
         num_retries = 0
         while num_retries < max_retries:
+            commons = {
+                "headers": headers,
+                "proxies": dict(http=cls.get_proxy(), https=cls.get_proxy()),
+                "timeout": 10
+            }
             try:
-                return requests.get(target_url, proxies=dict(http=cls.get_proxy(), https=cls.get_proxy()), timeout=10)
+                if method == 'GET':
+                    return requests.get(url, **commons)
+                else:
+                    return requests.post(url, data=json.dumps(body), **commons)
             except Exception as e:
                 print(e)
                 cls.proxy_index = (cls.proxy_index + 1) % len(cls.proxy_list)
                 num_retries += 1
-        return requests.get(target_url)
+        return requests.get(url)
 
 
-class ProxyView(View):
+class ProxyView(APIView):
 
-    def get(self, request, *args, **kwargs):
-        target_url = request.COOKIES.get('target_url', None)
-        if target_url:
-            response = ProxySwitcher.call_request(target_url)
+    def post(self, request, *args, **kwargs):
+        url = request.data.get("url", None)
+        headers = request.data.get("headers", None)
+        method = request.data.get("method", "GET")
+        if url:
+            response = ProxySwitcher.call_request(url, headers, method)
             headers = {"Location": response.headers.get("Location")} if response.headers.get("Location") else {}
             if response.headers.get("Content-Type"):
                 headers["Content-Type"] = response.headers.get("Content-Type")
             return HttpResponse(response.content, status=response.status_code, headers=headers)
         else:
-            return HttpResponse("Set target_url in cookie")
+            return Response({"error": "URL must be provided"}, status=200)
